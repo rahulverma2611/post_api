4de5331e7e88c9b85bba8e0593d59fff9d4a1a32
rahul <rv22090@gmail.com>
2024-06-25 10:36:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+import { useState } from "react";
+import axios from "axios";
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  function addUser() {
+    console.log(name, email, phone);
+    const user = { name, email, phone };
+    // fetch("http://localhost:3000/users", {
+    //   method: "post",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(user),
+    // }).then((resp) => {
+    //   console.log("response", resp);
+    // });
+
+    axios
+      .post("http://localhost:3000/users", user, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((save) => {
+        console.log("result:", save);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>post api practice</h2>
+      <input
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        type="name"
+      />{" "}
+      <br /> <br />
+      <input
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        type="email"
+      />{" "}
+      <br /> <br />
+      <input
+        value={phone}
+        onChange={(e) => {
+          setPhone(e.target.value);
+        }}
+        type="phone"
+      />{" "}
+      <br /> <br />
+      <button onClick={addUser} type="submit">
+        save user detail
+      </button>
     </div>
   );
 }
